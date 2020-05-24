df6ca70958108c98202bcfec998c843255a6b5b1
maurizio landolfi <landolfi@eutecna.com>
2020-05-24 16:11:26 +0200

    addes TestManutation class

---
+++ ManutationItemsApp.DAL/Migrations/20200524141017_TestManutation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManutationItemsApp.DAL.Migrations
+{
+    public partial class TestManutation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Suppliers",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
---
+++ ManutationItemsApp.Domain/Entities/TestManutation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ManutationItemsApp.Domain.Entities
+{
+    class TestManutation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int AssetId { get; set; }
+        [ForeignKey("AssetId")]
+        public Asset Asset  { get; set; }
+    }
+}
