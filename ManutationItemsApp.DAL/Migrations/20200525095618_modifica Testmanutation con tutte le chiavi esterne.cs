8816db65dfe4fbf0d53c0bf5eca9e67c05d68108
maurizio landolfi <landolfi@eutecna.com>
2020-05-25 11:57:17 +0200

    TestManutation con tutte le  chiavi esterne

---
+++ ManutationItemsApp.DAL/Migrations/20200525095618_modifica Testmanutation con tutte le chiavi esterne.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManutationItemsApp.DAL.Migrations
+{
+    public partial class modificaTestmanutationcontuttelechiaviesterne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorId",
+                table: "TestManutations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ErrorCodeId",
+                table: "TestManutations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ManutationTypeId",
+                table: "TestManutations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TypeOfFaultId",
+                table: "TestManutations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestManutations_CreatorId",
+                table: "TestManutations",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestManutations_ErrorCodeId",
+                table: "TestManutations",
+                column: "ErrorCodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestManutations_ManutationTypeId",
+                table: "TestManutations",
+                column: "ManutationTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestManutations_TypeOfFaultId",
+                table: "TestManutations",
+                column: "TypeOfFaultId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestManutations_AspNetUsers_CreatorId",
+                table: "TestManutations",
+                column: "CreatorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestManutations_ErrorCodes_ErrorCodeId",
+                table: "TestManutations",
+                column: "ErrorCodeId",
+                principalTable: "ErrorCodes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestManutations_ManutationTypes_ManutationTypeId",
+                table: "TestManutations",
+                column: "ManutationTypeId",
+                principalTable: "ManutationTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestManutations_typeOfFaults_TypeOfFaultId",
+                table: "TestManutations",
+                column: "TypeOfFaultId",
+                principalTable: "typeOfFaults",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestManutations_AspNetUsers_CreatorId",
+                table: "TestManutations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestManutations_ErrorCodes_ErrorCodeId",
+                table: "TestManutations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestManutations_ManutationTypes_ManutationTypeId",
+                table: "TestManutations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestManutations_typeOfFaults_TypeOfFaultId",
+                table: "TestManutations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestManutations_CreatorId",
+                table: "TestManutations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestManutations_ErrorCodeId",
+                table: "TestManutations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestManutations_ManutationTypeId",
+                table: "TestManutations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestManutations_TypeOfFaultId",
+                table: "TestManutations");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "TestManutations");
+
+            migrationBuilder.DropColumn(
+                name: "ErrorCodeId",
+                table: "TestManutations");
+
+            migrationBuilder.DropColumn(
+                name: "ManutationTypeId",
+                table: "TestManutations");
+
+            migrationBuilder.DropColumn(
+                name: "TypeOfFaultId",
+                table: "TestManutations");
+        }
+    }
+}
--- ManutationItemsApp.Domain/Entities/TestManutation.cs
+++ ManutationItemsApp.Domain/Entities/TestManutation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Text;
 
 namespace ManutationItemsApp.Domain.Entities
@@ -9,6 +10,18 @@ namespace ManutationItemsApp.Domain.Entities
     public class TestManutation
     {
         public int Id { get; set; }
+        public bool IsFailure { get; set; }
+        public bool IsCartolinaRossa { get; set; }
+        public bool IsOtherActivity { get; set; }
+        public bool NeedToAssign { get; set; }
+        public bool NotToDiplay { get; set; }
+        public bool Historical { get; set; }
+        public string CheckOutNote { get; set; }
+        [NotMapped]
+        
+        public DateTime DateOfCreation { get; set; }
+        public string BaseDescription { get; set; }
+        public string PauseReason { get; set; }
 
         [Required]
         public string Name { get; set; }
@@ -18,5 +31,21 @@ namespace ManutationItemsApp.Domain.Entities
         public int AssetId { get; set; }
         [ForeignKey("AssetId")]
         public Asset Asset  { get; set; }
+
+        public int ManutationTypeId { get; set; }
+        [ForeignKey("ManutationTypeId")]
+        public  ManutationTypess ManutationType { get; set; }
+
+        public string CreatorId { get; set; }
+        [ForeignKey("CreatorId")]
+        public virtual ApplicationUser Creator { get; set; }
+
+        public int ErrorCodeId { get; set; }
+        [ForeignKey("ErrorCodeId")]
+        public virtual ErrorCode ErrorCode { get; set; }
+
+        public int TypeOfFaultId { get; set; }
+        [ForeignKey("TypeOfFaultId")]
+        public TypeOfFault TypeOfFault { get; set; }
     }
 }
